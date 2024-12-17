4db935405246cdb0a93850ce66265909fedfae5a
Giovani Silva <giovani-silva1@live.com>
2024-12-16 22:08:57 -0300

    finalizado CRUD prato

--- Rasfood/src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ Rasfood/src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -8,12 +8,25 @@ public class PratoDao {
 
     private EntityManager entityManager;
 
-    public PratoDao(EntityManager entityManager){
+    public PratoDao(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar(Prato prato){
+    public void cadastrar(Prato prato) {
         entityManager.persist(prato);
         System.out.println("Prato salvo é:" + prato);
+    }
+
+    public Prato consultar(Integer id) {
+        return entityManager.find(Prato.class, id);
+    }
+
+
+    public void alterar(Prato prato) {
+        this.entityManager.merge(prato);
+    }
+
+    public void remover(Prato prato){
+        this.entityManager.remove(prato);
     }
 }
--- Rasfood/src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
+++ Rasfood/src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
@@ -12,18 +12,40 @@ public class PratoService {
 
         Prato risoto = new Prato();
         risoto.setNome("Risoto de Frutos do mar");
-        risoto.setNome("Risoto acompanhado de lula , polvo e mariscos");
+        risoto.setDescricao("Risoto acompanhado de lula , polvo e mariscos");
         risoto.setDisponivel(true);
         risoto.setValor(BigDecimal.valueOf(88.50));
+
+
+        Prato salmao = new Prato();
+        salmao.setNome("Salmao ao molho de maracuja");
+        salmao.setDescricao("Salmao grelhado ao molho de maracuja");
+        salmao.setDisponivel(true);
+        salmao.setValor(BigDecimal.valueOf(60.00));
+
 
         EntityManager entityManager = JPAUtil.getEntityManagerRasFood();
 
         PratoDao pratoDao = new PratoDao(entityManager);
         entityManager.getTransaction().begin();
         pratoDao.cadastrar(risoto);
-        entityManager.persist(risoto); // passando de transient para managed
+        pratoDao.cadastrar(salmao);
+        entityManager.persist(risoto);
+        entityManager.flush();
+        entityManager.persist(salmao);// passando de transient para managed
+        entityManager.flush();
+        System.out.println("O prato consultado foi:" + pratoDao.consultar(2));
+
+        pratoDao.remover(salmao);
+        System.out.println("O prato consultado foi depois de excluido:" + pratoDao.consultar(2));
+        entityManager.clear();
+
+        risoto.setValor(BigDecimal.valueOf(10000));
+        pratoDao.alterar(risoto);
+        System.out.println("O prato consultado foi:" + pratoDao.consultar(1));
         entityManager.getTransaction().commit();
         entityManager.close(); // passando para detached
+
 
     }
 }
